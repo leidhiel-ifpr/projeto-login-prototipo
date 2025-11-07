294cb17539bf1e2df27af4ccf0498e53efff2d63
leidhiel-ifpr <leidhiel.ifpr@gmail.com>
2025-11-07 17:59:58 +0000

    fgdgfd

---
+++ firebase-init.js
@@ -0,0 +1,15 @@
+ const firebaseConfig { 
+    apiKey: "AIzaSyB3XUYSBihLvKNzWZ7S9clGkPGE6lQ0Ykk",
+    authDomain: "projeto-login-prototipo.firebaseapp.com",
+    projectId: "projeto-login-prototipo",
+    storageBucket: "projeto-login-prototipo.firebasestorage.app",
+    messagingSenderId: "892885293201",
+    appId: "1:892885293201:web:465cc1aa3b3a8da6834730",
+    measurementId: "G-M0R89V9WT1"
+  };
+
+   
+  
+
+firebase.initializeApp(firebaseConfig); 
+  
